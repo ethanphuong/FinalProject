e5e6078694a008446ef5f2f1d21b56fbb76cc83a
vyvanvo <yvvanvo@gmail.com>
2020-11-24 13:21:20 -0800

    Created page test units
    * building CookBook
    * saving progress

--- app/src/test/java/com/example/cookingbythebook/ExampleUnitTest.kt
+++ app/src/test/java/com/example/cookingbythebook/ExampleUnitTest.kt
@@ -17,9 +17,35 @@ class ExampleUnitTest {
         assertEquals(4, 2 + 2)
     }
     @Test
-    fun page_title_isCorrect() {
-        var tempTitle: String = "Pizza"
-        var tempPage: Page = Recipe(tempTitle)
-        assertEquals(tempPage.returnTitle(), tempTitle)
+    fun return_page_title() {
+        var title: String = "Pizza"
+        var page: Page = Recipe(title)
+        assertEquals(page.returnTitle(), title)
+    }
+    @Test
+    fun change_page_title() {
+        var title: String = "Pizza"
+        var page: Page = Recipe(title)
+        assertEquals(page.returnTitle(), title)
+        //change title
+        var newTitle: String = "Supreme Pizza"
+        page.changeTitle(newTitle)
+        assertEquals(page.returnTitle(), newTitle)
+    }
+    @Test
+    fun title_compile_list() {
+        var cookBook: CookBook = CookBook("Vy's Cook Book")
+        var page1: Page = Recipe("Chocolate Cake")
+        var page2: Page = Recipe("Red Velvet Cake")
+        var page3: Page = Recipe("Brownie")
+        var page4: Page = Recipe("Cheesecake")
+        var page5: Page = Recipe("Mochi Donuts")
+        var page6: Page = Recipe("Glazed Donuts")
+        var page7: Page = Recipe("Tiramisu Cake")
+        var page8: Page = Recipe ("Vanilla Latte")
+        var page9: Page = Recipe("Chocolate Ice Cream")
+        var page10: Page = Recipe("Vanilla Ice Cream")
+
+
     }
 }
