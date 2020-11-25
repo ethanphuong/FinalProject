bdd481a0b20e2aee8baf6c7db666c5fc0edf878c
Ethan Phuong <ephuong@gmail.com>
2020-11-25 13:45:26 -0800

    updated iterator, not complete

--- app/src/main/java/com/example/cookingbythebook/iteratorpackage/IteratorClasses.kt
+++ app/src/main/java/com/example/cookingbythebook/iteratorpackage/IteratorClasses.kt
@@ -1,32 +1,48 @@
 package com.example.cookingbythebook.iteratorpackage
+
+import java.util.*
+
 package com.example.cookingbythebook.compositepackage
 import android.graphics.pdf.PdfDocument
 
 interface Iterator {
-    fun hasNext() : Boolean
-    fun hasPrev() : Boolean
     fun getNext() : Page
     fun getPrev() : Page
 }
 
-class Book {
-    private val pages = ArrayList<Page>()
+class Book:Cookbook {
+    inner class CookbookIterator() : Iterator {
+        override fun getNext(): Category
+        {
+        var titlePageIterator: Category = titlePage as Category
+        var categoryLengthNext: Int
+            categoryLengthNext = titlePageIterator.returnPagesCount()
+            var index: Int
+            index = pages[0]
+            if (categoryLengthNext >= 1)
+            {
+                for(i in 0..categoryLengthNext) {
+                    if (titlePage is Category) {
+                        return pages[index++]
+                    }
+                }
+            }
+        }
 
-    fun add(page: Page) {
-        pages.add(page)
-    }
-    class PageIterator(private val collection: pageList, var index : Int) : Iterator {
-        override fun getNext(): Page {
-            return collection.pages[index++]
-        }
-        override fun getPrev(): Page {
-            return collection.pages[index--]
-        }
-        override fun hasNext(): Page {
-            return index < collection.pages.size
-        }
-        override fun hasPrev() : Page {
-            return index > collection.pages.size
+        override fun getPrev(): Category
+        {
+            var titlePageIterator: Category = titlePage as Category
+            var categoryLengthPrev: Int
+            categoryLengthPrev = titlePageIterator.returnPagesCount()
+            var index: Int
+            index = pages.lastIndex
+            if ((categoryLengthPrev >= 1) {
+                        for(i in 0..categoryLengthPrev) {
+                            if (titlePage is Category) {
+                                return pages[index--]
+                            }
+                        }
+            }
         }
     }
 
