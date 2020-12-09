9940f70d4d47c7881773e25135186bccd36a2aab
Ethan Phuong <ephuong@gmail.com>
2020-12-09 13:54:50 -0800

    Merge branch 'ephuong/iterator-package' of https://github.com/cs100/final-project-ephuo001-btran117-vvo025
    # Conflicts:
    #	.idea/gradle.xml
    #	app/src/main/java/com/example/cookingbythebook/MainActivity.kt
    #	app/src/main/res/layout/activity_main.xml
    #	app/src/main/res/values/strings.xml

--- app/src/main/java/com/example/cookingbythebook/iteratorpackage/IteratorClasses.kt
+++ app/src/main/java/com/example/cookingbythebook/iteratorpackage/IteratorClasses.kt
@@ -1,6 +1,6 @@
 package com.example.cookingbythebook.iteratorpackage
 
-import java.util.*
+import java.util.Stack
 
 import com.example.cookingbythebook.compositepackage.Category
 import com.example.cookingbythebook.compositepackage.Page
@@ -9,75 +9,99 @@ import com.example.cookingbythebook.compositepackage.Page
 import android.graphics.pdf.PdfDocument
 
 interface Iterator {
-    fun right(): Page
-    fun left(): Page
-    fun getCurrent(): Page
+    fun getNext()
+    fun isDone(): Boolean
+    fun first()
+}
+interface CategoryIteratorInterface : Iterator{
+    var atEnd: Boolean
+    fun current(): Page
 }
 
+interface PreorderIteratorInterface : Iterator {
+    var iterators: Stack<Iterator>
+    fun current(): Iterator
+}
 
-class BinaryIterator(var arr: MutableList<Page>, var index : Int) : Iterator {
-    override fun right(): Page {
-        return if(index < arr.size)
-        {
-            var temp: Int = index
-            temp += 1
-            arr[temp]
-        }
-        else {
-            arr[index]
+class CategoryIterator(var arr: ArrayList<Page>, var index : Int) : Iterator, CategoryIteratorInterface {
+
+    override var atEnd: Boolean = false
+
+    constructor(){
+        index = 0
+    }
+    override fun getNext() {
+        when {
+            index < arr.size -> {
+                var temp: Int = index
+                temp += 1
+                index = temp
+            }
+            index == arr.size - 1 -> {
+                atEnd = true
+            }
+            else -> {
+                atEnd = true
+            }
         }
     }
 
-    override fun left(): Page {
-        return if(index > 0)
+    override fun isDone(): Boolean {
+        if(atEnd)
         {
-            var temp2: Int = index
-            temp2 -= 1
-            arr[temp2]
+            return true
         }
-        else {
-            arr[index]
-        }
+        return false
     }
 
-    override fun getCurrent(): Page {
+    override fun first() {
+        index = 0
+    }
+
+    override fun current(): Page {
         return arr[index]
     }
 }
 
-class PreorderIterator(var arr: MutableList<Page>) : Iterator {
-    override fun right() : Page {
-        val binaryRight = BinaryIterator(arr, 0)
-        val size: Int = arr.size - 1
-        var x: Int = 0
-        while (x < size) {
-            if (arr[x] is Page) {
-                binaryRight.right()
-                x++
-            } else {
-                x++
-            }
-        }
-        return arr[x]
+class PreorderIterator(var titlePage: Page?) : Iterator, PreorderIteratorInterface {
+
+    override var iterators: Stack<Iterator> = Stack<Iterator>()
+
+    constructor(){
+        iterators = Stack<Iterator>()
     }
 
-    override fun left() : Page{
-        val binaryLeft = BinaryIterator(arr, arr.size - 1)
-        val sizeLeft: Int = arr.size - 1
-        var xLeft: Int = 0
-        while (xLeft < sizeLeft) {
-            if (arr[xLeft] is Page) {
-                binaryLeft.left()
-                xLeft++
-            } else {
-                xLeft++
-            }
+    override fun first(){
+        while(!iterators.empty())
+        {
+            iterators.pop()
         }
-        return arr[xLeft]
+        var rootIterator = Category.createIterator()
+        rootIterator.first()
+        iterators.push(rootIterator)
     }
 
-    override fun getCurrent() : Page {
-        return arr[0]
+    override fun getNext() {
+        var topIterator: Iterator = current()
+        topIterator = Category.createIterator()
+        topIterator.first()
+        iterators.push(topIterator)
+        while(!iterators.empty() && iterators.peek().isDone())
+        {
+            iterators.pop()
+            if(!iterators.empty())
+            {
+                iterators.peek().getNext()
+            }
+        }
+    }
+
+    override fun isDone(): Boolean {
+        return iterators.empty()
+    }
+
+    override fun current() : Iterator {
+        return iterators.peek()
     }
 }
 
