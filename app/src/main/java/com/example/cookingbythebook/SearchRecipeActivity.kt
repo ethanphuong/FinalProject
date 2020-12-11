7e7ba805cb32bfc41f79b726d274fcd6db30724b
vyvanvo <yvvanvo@gmail.com>
2020-12-11 00:19:56 -0800

    Created Search Recipe Page

---
+++ app/src/main/java/com/example/cookingbythebook/SearchRecipeActivity.kt
@@ -0,0 +1,11 @@
+package com.example.cookingbythebook
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SearchRecipeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search_recipe)
+    }
+}
